f0d1be68be4d74012c1f290aadb5471f9cd29ca2
rasoulazizi <rasoulazizi@outlook.com>
2026-02-12 00:35:14 +0330

    create plot from iran gdp by year

---
+++ gdp_plot.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_csv('data.csv')
+
+years = data['Year'].tolist()
+gdp = data['GDP_Current_USD'].tolist()
+
+gdp_billion = [i/1000000000 for i in gdp]
+
+plt.plot(years, gdp_billion, color='red', marker='o')
+plt.title('iran GDP by year')
+plt.xlabel('Years')
+plt.ylabel('USD(Billion USD)')
+plt.grid(True)
+plt.show()
