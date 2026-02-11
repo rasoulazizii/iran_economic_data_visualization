67607105a33b0bf07d4173fea9145a4245862ec6
rasoulazizi <rasoulazizi@outlook.com>
2026-02-12 00:38:44 +0330

    create iran inflation by year plot

---
+++ inflation_plot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('data.csv')
+years = data['Year'].tolist()
+inflation = data['Inflation_Rate_Percent'].tolist()
+
+plt.plot(years, inflation, color='red', marker='o')
+plt.title('Iran inflation by year')
+plt.xlabel('Years')
+plt.ylabel('Inflation(Percent)')
+plt.grid(True)
+plt.show()
