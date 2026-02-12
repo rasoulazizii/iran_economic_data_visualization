b203032efab08887c09075b109ba43ed9ce8a84a
rasoulazizi <rasoulazizi@outlook.com>
2026-02-12 13:50:29 +0330

    make better plot in gdp vs inflation

--- gdp_inflation.py
+++ gdp_inflation.py
@@ -10,14 +10,23 @@ gdp_raw = data['GDP_Current_USD'].tolist()
 gdp_billion = [i/1000000000 for i in gdp_raw]
 
 
-fig, ax1 = plt.subplots()
+fig, ax1 = plt.subplots(figsize=(10,6))
 
-ax1.plot(years, gdp_billion, label='GDP', color='red')
-ax1.set_ylabel('GDP (Billion current USD)')
+ax1.plot(years, gdp_billion, marker='o', color='red', label='GDP')
+ax1.set_ylabel('GDP (Billion USD)')
+ax1.grid(True, linestyle='--', alpha=0.5)
 
 ax2 = ax1.twinx()
-ax2.plot(years, inflation, label='Inflation', color='blue')
+ax2.plot(years, inflation, marker='s', color='blue', label='Inflation')
 ax2.set_ylabel('Inflation (%)')
 
 plt.title('GDP vs Inflation')
+plt.xticks(rotation=45)
+
+ax1.legend(loc='upper left')
+ax2.legend(loc='upper right')
+
+plt.tight_layout()
+
+
 plt.show()
