febdc0a44388c59485ff0fcd7ee5c4d895106e34
rasoulazizi <rasoulazizi@outlook.com>
2026-02-12 17:14:34 +0330

    iran trade balance by yaer

---
+++ trade_balance.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data.csv')
+
+
+years = data['Year'].tolist()
+
+trade_balance = (data['Exports_USD'] - data['Imports_USD']) / 1_000_000_000
+
+
+plt.figure(figsize=(10,6))
+
+plt.plot(years, trade_balance, marker='o', linewidth=2, label='trade balance')
+plt.title('Trade Balance')
+plt.xlabel('years')
+plt.ylabel('Trade Balance (Billion USD)')
+plt.legend(loc='upper left', fontsize=10)
+plt.grid(True)
+
+plt.show()
